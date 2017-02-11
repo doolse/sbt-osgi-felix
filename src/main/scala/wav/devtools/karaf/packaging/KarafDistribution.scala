c72e8e6cf4cbe2336486b475bcfb32e791c9559a
Philip Andrew <phil@orsa-studio.com>
2017-02-11 20:56:23 +0800

    ...

--- src/main/scala/wav/devtools/karaf/packaging/KarafDistribution.scala
+++
@@ -1,8 +0,0 @@
-package wav.devtools.karaf.packaging
-
-import java.net.URI
-
-case class KarafDistribution(url: URI, artifactName: String, contentPath: String) {
-  override def toString(): String =
-    s"KarafDistribution($url,$artifactName,$contentPath)"
-}
